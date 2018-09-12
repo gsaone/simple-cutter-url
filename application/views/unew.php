5ddc13e5e3c40ea9c946e2f4b07991072c7b4976
gsaone <gsaone@users.noreply.github.com>
2018-09-13 01:03:02 +0300

    Update unew.php

--- application/views/unew.php
+++ application/views/unew.php
@@ -3,13 +3,13 @@
    <head>
       <meta charset="utf-8">
       <title>
-         Пример обрезателя ссылок
+         Simple cutter url
       </title>
    </head>
    <body><center>
-   <h2>Ваша сокращенная ссылка:</h2>
+   <h2>Your link:</h2>
    <p><a href="<?php print $add; ?>" target="_blank"><?php print $add; ?></a></p>
-   <p><a href="<?php print base_url(); ?>"><h4><НАЗАД</h4></a></p>
+   <p><a href="<?php print base_url(); ?>"><h4><Backward</h4></a></p>
    </center></body>
 </html>
 
